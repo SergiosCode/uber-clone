fe638bcd96a23263ea56d2291e0224d4eda73b6f
Sergio Valencia <Streameraudio@yahoo.com>
2021-11-05 19:36:50 -0700

    added dynamic map markers

--- pages/components/Map.js
+++ pages/components/Map.js
@@ -4,21 +4,38 @@ import tw from "tailwind-styled-components/dist/tailwind";
 import mapboxgl from "!mapbox-gl";
 
 mapboxgl.accessToken = "pk.eyJ1Ijoic3ZhbGVuY2lhOTYiLCJhIjoiY2t2bG85NWc0YzV1YTJwdDl2eGo0ZXY0eiJ9.qOWuNC71r_8dyu54nVmhLA";
-const map = () => {
-    useEffect(() => {
-        const map = new mapboxgl.Map({
-          container: "map",
-          style: "mapbox://styles/drakosi/ckvcwq3rwdw4314o3i2ho8tph",
-          center: [-99.29011, 39.39172],
-          zoom: 3,
-        });
+const map = (props) => {
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/drakosi/ckvcwq3rwdw4314o3i2ho8tph",
+      center: [-99.29011, 39.39172],
+      zoom: 3,
+    });
+
+    if (props.pickupCoordinates) {
+      addToMap(map, props.pickupCoordinates);
+    }
+
+    if (props.dropoffCoordinates) {
+      addToMap(map, props.dropoffCoordinates);
+    }
+
+    if (props.pickupCoordinates && props.dropoffCoordinates) {
+      map.fitBounds([props.pickupCoordinates, props.dropoffCoordinates], {
+        padding: 60,
       });
-  return <Wrapper id='map'></Wrapper>;
+    }
+  }, [props.pickupCoordinates, props.dropoffCoordinates]);
+
+  const addToMap = (map, coordinates) => {
+    const marker1 = new mapboxgl.Marker().setLngLat(coordinates).addTo(map);
+  };
+
+  return <Wrapper id="map"></Wrapper>;
 };
 
 export default map;
 
 const Wrapper = tw.div`
-flex-1`
-
-
+flex-1 h-1/2`;
